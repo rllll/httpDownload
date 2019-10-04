3e45d53eb80f3d2d76628e51166e18aca0434cf1
wenyao liu <Liuwenyao2017@126.com>
2019-10-04 12:46:38 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace httpDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.recevMsg = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.DateEnd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.filecombine = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.startDownload = new System.Windows.Forms.Button();
+            this.Date = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.recvThreadnum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SourceUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.recevMsg);
+            this.groupBox1.Location = new System.Drawing.Point(29, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 468);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "接收信息";
+            // 
+            // recevMsg
+            // 
+            this.recevMsg.FormattingEnabled = true;
+            this.recevMsg.ItemHeight = 12;
+            this.recevMsg.Location = new System.Drawing.Point(16, 31);
+            this.recevMsg.Name = "recevMsg";
+            this.recevMsg.Size = new System.Drawing.Size(432, 412);
+            this.recevMsg.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.DateEnd);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.filecombine);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.startDownload);
+            this.groupBox2.Controls.Add(this.Date);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.recvThreadnum);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.path);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.SourceUrl);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(522, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 468);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "开始区";
+            // 
+            // DateEnd
+            // 
+            this.DateEnd.Enabled = false;
+            this.DateEnd.Location = new System.Drawing.Point(125, 266);
+            this.DateEnd.Name = "DateEnd";
+            this.DateEnd.Size = new System.Drawing.Size(199, 21);
+            this.DateEnd.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(11, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "接收结束时间";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // filecombine
+            // 
+            this.filecombine.Location = new System.Drawing.Point(238, 371);
+            this.filecombine.Name = "filecombine";
+            this.filecombine.Size = new System.Drawing.Size(86, 23);
+            this.filecombine.TabIndex = 11;
+            this.filecombine.Text = "合并文件";
+            this.filecombine.UseVisualStyleBackColor = true;
+            this.filecombine.Click += new System.EventHandler(this.Filecombine_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "清空接收消息";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 21);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // startDownload
+            // 
+            this.startDownload.Location = new System.Drawing.Point(238, 318);
+            this.startDownload.Name = "startDownload";
+            this.startDownload.Size = new System.Drawing.Size(86, 23);
+            this.startDownload.TabIndex = 8;
+            this.startDownload.Text = "下载";
+            this.startDownload.UseVisualStyleBackColor = true;
+            this.startDownload.Click += new System.EventHandler(this.StartDownload_Click);
+            // 
+            // Date
+            // 
+            this.Date.Enabled = false;
+            this.Date.Location = new System.Drawing.Point(125, 219);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(199, 21);
+            this.Date.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(11, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "接收开始时间";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // recvThreadnum
+            // 
+            this.recvThreadnum.Location = new System.Drawing.Point(125, 168);
+            this.recvThreadnum.Name = "recvThreadnum";
+            this.recvThreadnum.Size = new System.Drawing.Size(199, 21);
+            this.recvThreadnum.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(24, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "接收线程数";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(125, 110);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(162, 21);
+            this.path.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(24, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "存储位置";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SourceUrl
+            // 
+            this.SourceUrl.Location = new System.Drawing.Point(125, 55);
+            this.SourceUrl.Name = "SourceUrl";
+            this.SourceUrl.Size = new System.Drawing.Size(199, 21);
+            this.SourceUrl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(24, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "下载网址";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 547);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "多线程下载";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox SourceUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Date;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox recvThreadnum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startDownload;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox recevMsg;
+        private System.Windows.Forms.Button filecombine;
+        private System.Windows.Forms.TextBox DateEnd;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Threading;
+//测试网址http://mirrors.163.com/debian/ls-lR.gz
+//大文件http://kd.269.net/200.zip
+namespace httpDownload
+{
+    public partial class Form1 : Form
+    {
+        public bool[] threadendTag;//判断各线程是否接收完文件
+        public string[] filenameList;//各线程下载的小文件名列表
+        public int[] filestartpos;//各线程开始读取文件的位置
+        public int[] filesize;//小文件大小
+        public string url;//请求链接
+        public bool union;//判断文件是否合并完成
+        public int thread;//线程数
+        /*public bool ThreadDead;*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            //path.Text = "D:\\VS2019Project\\httpDownload\\download";
+            String fullPath = System.Environment.CurrentDirectory.ToString();
+            
+            path.Text = Directory.GetParent(Directory.GetParent(fullPath).FullName).FullName + "\\download";
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.Description = "请选择一个下载目录";
+            string Path;
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                Path = fb.SelectedPath;
+                path.Text = Path;
+            }
+        }
+        public class HttpFile
+        {
+            public Form1 formm;//声明一个窗体类
+            public int threadIndex;//进程标识
+            public string filenamea;//下载的小文件名
+            public string strUrl;//请求的链接
+            public FileStream fs;
+            public HttpWebRequest request;
+            public Stream ns;
+            public byte[] nbytes;//接收缓冲区
+            public int nreadsize;//接收字节数
+            
+
+            public HttpFile(Form1 form, int thread)
+            {
+                formm = form;
+                threadIndex = thread;
+            }
+            ~HttpFile()
+            {
+                formm.Dispose();
+            }
+            public int getThread
+            {
+                get
+                {
+                    return threadIndex;
+                }
+            }
+            public int getFilesize
+            {
+                get
+                {
+                    return formm.filesize[threadIndex];
+                }
+            }
+            public void receive()//线程开始接收文件
+            {
+                filenamea = formm.filenameList[threadIndex];
+                strUrl = formm.url;
+                ns = null;
+                nbytes = new byte[512];
+                nreadsize = 0;
+                formm.recevMsg.Items.Add("线程" + threadIndex.ToString() + "开始接收：" + "文件大小" + Math.Ceiling(formm.filesize[threadIndex] / 1024.0f) + "KB");
+                fs = new FileStream(filenamea,FileMode.Create);
+
+                try
+                {
+                    request = (HttpWebRequest)HttpWebRequest.Create(strUrl);
+                    //要接收文件的字节范围
+                    request.AddRange(formm.filestartpos[threadIndex], formm.filestartpos[threadIndex] + formm.filesize[threadIndex]);
+                    ns = request.GetResponse().GetResponseStream();
+                    nreadsize = ns.Read(nbytes, 0, nbytes.Length);
+                    while (nreadsize > 0)
+                    {
+                        fs.Write(nbytes, 0, nreadsize);
+                        //formm.recevMsg.Items.Add("线程" + threadIndex.ToString() + "已下载字节：" + Math.Ceiling(nreadsize / 1024.0f) + "KB");
+                        nreadsize = ns.Read(nbytes, 0, nbytes.Length);
+                    }
+                    fs.Close();
+                    ns.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "提示");
+                    fs.Close();
+                }
+                formm.recevMsg.Items.Add("线程" + threadIndex.ToString() + "接收完毕！");
+                
+                
+            }
+        }
+ 
+        private void StartDownload_Click(object sender, EventArgs e)
+        {
+            
+            DateTime date = DateTime.Now;
+            Date.Text = date.ToString();
+            url = SourceUrl.Text.ToString();
+            HttpWebRequest request;
+            long filesizew = 0;
+            try
+            {
+                request = (HttpWebRequest)HttpWebRequest.Create(url);//建立一个HTTP请求
+                filesizew = request.GetResponse().ContentLength;//得到文件总大小
+                request.Abort();//取消请求
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("错误：" + ex.Message,"错误信息");
+                return;
+            }
+
+            thread = Convert.ToInt32(recvThreadnum.Text.Trim().ToString(), 10);
+            recevMessageShow("同时下载线程数：" + thread.ToString());
+            recevMessageShow("文件大小：" + Math.Ceiling(filesizew / 1024.0f) + "KB");
+
+            threadendTag = new bool[thread];
+            filenameList = new string[thread];
+            filestartpos = new int[thread];
+            filesize = new int[thread];
+
+            int filethread = (int)filesizew / thread;
+            int filethreade = filethread + (int)filesizew % thread;
+            string filePath = path.Text;
+
+            //文件大小切割
+            for (int i = 0; i < thread; i++)
+            {
+                threadendTag[i] = false;
+                filenameList[i] = filePath + "\\" + i.ToString() + ".txt";
+                if (i < thread - 1)
+                {
+                    filestartpos[i] = filethread * i;
+                    filesize[i] = filethread - 1;
+                }
+                else
+                {
+                    filestartpos[i] = filethread * i;
+                    filesize[i] = filethreade - 1;
+                }
+            }
+            
+            Thread[] threadk = new Thread[thread];
+            HttpFile[] httpfile = new HttpFile[thread];
+
+            //各进程开始下载
+            for (int j = 0; j < thread; j++)
+            {
+                httpfile[j] = new HttpFile(this, j);
+                threadk[j] = new Thread(new ThreadStart(httpfile[j].receive));
+                threadk[j].Start();
+            }
+            
+
+        }
+        public void Combine()
+        {
+            /*for (int i = 0; i < thread; i++)
+                threadendTag[i] = false;*/
+            while (true)
+            {
+                union = true;
+                for (int i = 0; i < thread; i++)
+                {
+                    if (threadendTag[i] == false)
+                    {
+                        union = false;
+                        Thread.Sleep(100);
+                        break;
+                    }
+                }
+                if (union == true)
+                {
+                    break;
+                }
+                string[] fns = SourceUrl.Text.Split('/');
+                string saveFilePath = path.Text + "\\" + fns[fns.Length - 1];
+                FileStream fs = new FileStream(saveFilePath, FileMode.Create);
+                FileStream fstemp;
+                byte[] bytes = new byte[512];
+                int readsize;
+                for (int i = 0; i < thread; i++)
+                {
+                    fstemp = new FileStream(filenameList[i], FileMode.Open);
+                    readsize = fstemp.Read(bytes, 0, bytes.Length);
+                    while (readsize > 0)
+                    {
+                        fs.Write(bytes, 0, readsize);
+                        readsize = fstemp.Read(bytes, 0, bytes.Length);
+                    }
+                    threadendTag[i] = true;
+                    fstemp.Close();
+                }
+                long recevfilesize = fs.Length;
+                fs.Close();
+                recevMsg.Items.Add("已成功接收的文件总大小：" + Math.Ceiling(recevfilesize / 1024.0f) + "KB");
+            }
+            DateTime date = DateTime.Now;
+            DateEnd.Text = date.ToString();
+            if (MessageBox.Show("是否删除缓存？","提示",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                for (int i = 0; i < thread; i++)
+                {
+                    FileInfo fi = new FileInfo(filenameList[i]);
+                    try
+                    {
+                        fi.Delete();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+        }
+
+        private void recevMessageShow(string Message)
+        {
+            recevMsg.Items.Add(Message);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            recevMsg.Items.Clear();
+        }
+
+        private void Filecombine_Click(object sender, EventArgs e)
+        {
+
+            Thread hbth = new Thread(new ThreadStart(Combine));
+            hbth.Start();
+        }
+    }
+}
